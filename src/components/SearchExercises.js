29bc1c271f0f7ddc3c92043ccff1c5f3d45875fe
Musa Liman <musaliman27@gmail.com>
2022-09-15 12:59:48 +0100

    search function and scrollbar add

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { excerciseOptions, fetchData } from '../utils/FetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
 
 
