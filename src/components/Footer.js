8f2d916035f993b710ddd7ef48bcaa2884b4315f
Musa Liman <musaliman27@gmail.com>
2022-09-16 19:08:32 +0100

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Typography, Box} from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt={Logo} width='200px' height='50px'/>
+        <Typography variant='h5' mt='20px' mb='20px'>
+          Fitness webapp created for <span color='red'>beast</span> in you &copy; Bear Gym
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
